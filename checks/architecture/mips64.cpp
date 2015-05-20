21436db56a45b8962b0289434b77e445d4208ccd
Robert Schiele <rschiele@gmail.com>
2015-05-20 08:25:53 +0200

    deduce mips64 architecture

---
+++ checks/architecture/mips64.cpp
@@ -0,0 +1,11 @@
+// mips64.cpp
+//
+// Copyright (c) 2012 Steven Watanabe
+//
+// Distributed under the Boost Software License Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#if !(defined(__mips64))
+#error "Not MIPS64"
+#endif
